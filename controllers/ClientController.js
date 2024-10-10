57d7905029d3e9fdffe2e0ddca05b977c5fce608
DCODER <dlozanoc862@gmail.com>
2024-10-10 11:28:23 -0500

    ♻️ REFACTOR - Esquema de la api rest clientes

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -5,33 +5,71 @@ import {
     getClientsQuery,
     updateClientQuery
 } from "../DB/ClientQuery.js";
+import { successAnswer } from "../helpers/answersApi.js";
 
 const TABLE = 'clientes';
 
-const getClientsController = () => {
-    return getClientsQuery(TABLE);
+//Obtiene los datos de todos los clientes de la BD
+const getClients = async(req, res, next) => {
+
+    try {
+        const getClients = await getClientsQuery(TABLE);
+        successAnswer(req, res, getClients, 200);
+    } catch (error) {
+        next(error);
+    }
 }
 
-const getClientByIdController = (id) => {
-    return getClientByIdQuery(TABLE, id);
+//Obtiene los datos de un solo cliente de la BD
+const getClientById = async(req, res, next) => {
+
+    try {
+        const getClients = await getClientByIdQuery(TABLE, req.params.id);
+        successAnswer(req,res, getClients, 200);
+    } catch (error) {
+        next(error);
+    }
+
 }
 
-const createClientController = (body) => {
-    return createClientQuery(TABLE, body);
+//Elimina un  cliente
+const deleteClientById = async(req, res, next) => {
+
+    try {
+        const item = await deleteClientQuery(TABLE, req.body);
+        successAnswer(req,res, 'Cliente eliminado correctamente', 200);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+
 }
 
-const updateClientController = (id, body) => {
-    return updateClientQuery(TABLE, body, id);
-}
+//Agrega o actualiza un cliente
+const createOrUpdateClient = async (req, res, next) => {
+    try {
+        let message = '';
+        const { id, ...clientData } = req.body;
 
-const deleteClientController = (body) => {
-    return deleteClientQuery(TABLE, body);
+        if (id === 0 || !id) {
+            // Crear cliente si no hay ID o si es 0
+            const newItem = await createClientQuery(TABLE, clientData);
+            message = 'Cliente creado con éxito';
+        } else {
+            // Actualizar cliente si hay un ID
+            const updatedItem = await updateClientQuery(TABLE, clientData, id);
+            message = 'Cliente actualizado con éxito';
+        }
+        successAnswer(req, res, message, 201);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 }
 
 export{
-    getClientsController,
-    getClientByIdController,
-    createClientController,
-    updateClientController,
-    deleteClientController
+    deleteClientById,
+    createOrUpdateClient,
+    getClientById,
+    getClients
 }
--- routes/ClientRoute.js
+++ routes/ClientRoute.js
@@ -1,71 +1,10 @@
 import express from "express";
-import { successAnswer } from "../helpers/answersApi.js";
 import {
-    createClientController,
-    deleteClientController,
-    getClientByIdController,
-    getClientsController,
-    updateClientController }
+    deleteClientById,
+    createOrUpdateClient,
+    getClientById,
+    getClients }
 from "../controllers/ClientController.js";
-
-//Obtiene los datos de todos los clientes de la BD
-const getClients = async(req, res, next) => {
-
-    try {
-        const getClients = await getClientsController();
-        successAnswer(req, res, getClients, 200);
-    } catch (error) {
-        next(error);
-    }
-}
-
-//Obtiene los datos de un solo cliente de la BD
-const getClientById = async(req, res, next) => {
-
-    try {
-        const getClients = await getClientByIdController(req.params.id);
-        successAnswer(req,res, getClients, 200);
-    } catch (error) {
-        next(error);
-    }
-
-}
-
-//Elimina un  cliente
-const deleteClientById = async(req, res, next) => {
-
-    try {
-        const item = await deleteClientController(req.body);
-        successAnswer(req,res, 'Cliente eliminado correctamente', 200);
-    } catch (error) {
-        console.log(error);
-        next(error);
-    }
-
-}
-
-//Agrega o actualiza un cliente
-const createOrUpdateClient = async (req, res, next) => {
-    try {
-        let message = '';
-        const { id, ...clientData } = req.body;
-
-        if (id === 0 || !id) {
-            // Crear cliente si no hay ID o si es 0
-            const newItem = await createClientController(clientData);
-            message = 'Cliente creado con éxito';
-        } else {
-            // Actualizar cliente si hay un ID
-            const updatedItem = await updateClientController(id, clientData);
-            message = 'Cliente actualizado con éxito';
-        }
-
-        successAnswer(req, res, message, 201);
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-}
 
 
 //Rutas
