52a28d912c0caf424e99898e061e56af3e325aa2
DCODER <dlozanoc862@gmail.com>
2024-10-10 15:57:27 -0500

    ✨ ADD - Seguridad y autorizacion a los recursos de la api

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -5,13 +5,14 @@ import {
     getAllUsersController,
     getUserByIdController
 } from "../controllers/UserController.js";
+import { authenticateToken, authorizeRole } from "../middleware/authMiddleware.js";
 
 
 //Rutas
 const router = express.Router();
-router.get("/", getAllUsersController);
-router.get("/:id", getUserByIdController);
-router.put('/', deleteUserController)
-router.post('/', createOrUpdateUserController)
+router.get("/", authenticateToken, authorizeRole('admin'), getAllUsersController);
+router.get("/:id", authenticateToken, authorizeRole('admin'), getUserByIdController);
+router.put('/', authenticateToken, authorizeRole('admin'), deleteUserController)
+router.post('/', authenticateToken, authorizeRole('admin'), createOrUpdateUserController)
 
 export default router;
