3dbd6d4890a4948efbfc23923e39d001af9291c5
DCODER <dlozanoc862@gmail.com>
2025-01-18 15:51:20 -0500

    🎨 Se crea la estructura controlador de categorias y subcategorias

--- src/categorias/CategoriesController.js
+++ src/categorias/CategoriesController.js
@@ -1,60 +1,47 @@
 import { successAnswer } from "../../helpers/answersApi.js";
-import { 
-    createCategoryQuery, 
-    deleteCategoryQuery, 
-    getCategoryByIdQuery, 
-    getCategoriesQuery, 
-    updateCategoryQuery,
-    createSubcategoryQuery,
-    deleteSubcategoryQuery,
-    getSubcategoryByIdQuery,
-    getSubcategoriesQuery,
-    updateSubcategoryQuery
-} from "./CategoriesQuery.js";
-
-const CATEGORY_TABLE = 'categorias';
-const SUBCATEGORY_TABLE = 'subcategorias';
+import Categoria from "./CategoriaModel.js";
+import Subcategoria from "./SubCategoriesModel.js";
 
 // Controlador para Categorías
 
-const getCategories = async(req, res, next) => {
+const getCategories = async (req, res, next) => {
     try {
-        const categories = await getCategoriesQuery(CATEGORY_TABLE);
+        const categories = await Categoria.findAll();
         successAnswer(req, res, categories, 200);
     } catch (error) {
         next(error);
     }
 }
 
-const getCategoryById = async(req, res, next) => {
+const getCategoryById = async (req, res, next) => {
     try {
-        const category = await getCategoryByIdQuery(CATEGORY_TABLE, req.params.id);
+        const category = await Categoria.findByPk(req.params.id);
         successAnswer(req, res, category, 200);
     } catch (error) {
         next(error);
     }
 }
 
-const deleteCategoryById = async(req, res, next) => {
+const deleteCategoryById = async (req, res, next) => {
     try {
         const categoryId = req.params.id;
-        await deleteCategoryQuery(CATEGORY_TABLE, categoryId);
+        await Categoria.destroy({ where: { id_categoria: categoryId } });
         successAnswer(req, res, 'Categoría eliminada correctamente', 200);
     } catch (error) {
         next(error);
     }
 }
 
-const createOrUpdateCategory = async(req, res, next) => {
+const createOrUpdateCategory = async (req, res, next) => {
     try {
         let message = '';
         const { id_categoria, ...categoryData } = req.body;
 
-        if (id_categoria === 0 || !id_categoria) {
-            await createCategoryQuery(CATEGORY_TABLE, categoryData);
+        if (!id_categoria || id_categoria === 0) {
+            await Categoria.create(categoryData);
             message = 'Categoría creada con éxito';
         } else {
-            await updateCategoryQuery(CATEGORY_TABLE, categoryData, id_categoria);
+            await Categoria.update(categoryData, { where: { id_categoria } });
             message = 'Categoría actualizada con éxito';
         }
         successAnswer(req, res, message, 201);
@@ -65,44 +52,44 @@ const createOrUpdateCategory = async(req, res, next) => {
 
 // Controlador para Subcategorías
 
-const getSubcategories = async(req, res, next) => {
+const getSubcategories = async (req, res, next) => {
     try {
-        const subcategories = await getSubcategoriesQuery(SUBCATEGORY_TABLE);
+        const subcategories = await Subcategoria.findAll();
         successAnswer(req, res, subcategories, 200);
     } catch (error) {
         next(error);
     }
 }
 
-const getSubcategoryById = async(req, res, next) => {
+const getSubcategoryById = async (req, res, next) => {
     try {
-        const subcategory = await getSubcategoryByIdQuery(SUBCATEGORY_TABLE, req.params.id);
+        const subcategory = await Subcategoria.findByPk(req.params.id);
         successAnswer(req, res, subcategory, 200);
     } catch (error) {
         next(error);
     }
 }
 
-const deleteSubcategoryById = async(req, res, next) => {
+const deleteSubcategoryById = async (req, res, next) => {
     try {
         const subcategoryId = req.params.id;
-        await deleteSubcategoryQuery(SUBCATEGORY_TABLE, subcategoryId);
+        await Subcategoria.destroy({ where: { id_subcategoria: subcategoryId } });
         successAnswer(req, res, 'Subcategoría eliminada correctamente', 200);
     } catch (error) {
         next(error);
     }
 }
 
-const createOrUpdateSubcategory = async(req, res, next) => {
+const createOrUpdateSubcategory = async (req, res, next) => {
     try {
         let message = '';
         const { id_subcategoria, ...subcategoryData } = req.body;
 
-        if (id_subcategoria === 0 || !id_subcategoria) {
-            await createSubcategoryQuery(SUBCATEGORY_TABLE, subcategoryData);
+        if (!id_subcategoria || id_subcategoria === 0) {
+            await Subcategoria.create(subcategoryData);
             message = 'Subcategoría creada con éxito';
         } else {
-            await updateSubcategoryQuery(SUBCATEGORY_TABLE, subcategoryData, id_subcategoria);
+            await Subcategoria.update(subcategoryData, { where: { id_subcategoria } });
             message = 'Subcategoría actualizada con éxito';
         }
         successAnswer(req, res, message, 201);
@@ -120,4 +107,4 @@ export {
     getSubcategoryById,
     deleteSubcategoryById,
     createOrUpdateSubcategory
-}
+};
